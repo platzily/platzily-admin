f42891ed572be2f95384df0b721bbbda45f9d210
Cesar Augusto Puentes Sandoval <42822751+cesarp04@users.noreply.github.com>
2021-11-29 20:39:17 -0500

    Protected routes and connection with front application

--- packages/api-gw/src/index.js
+++ packages/api-gw/src/index.js
@@ -1,26 +1,46 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: { level: "info" } });
-var jwt = require("fastify-jwt");
-var jwks = require("jwks-rsa");
+// var jwt = require("fastify-jwt");
+// var jwks = require("jwks-rsa");
 
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: "https://dev-q-csylwc.us.auth0.com/.well-known/jwks.json",
-  }),
+fastify.register(require("fastify-auth0-verify"), {
+  domain: "dev-q-csylwc.us.auth0.com",
   audience: "admin-api-platzily.com",
-  issuer: "https://dev-q-csylwc.us.auth0.com/",
-  algorithms: ["RS256"],
 });
 
-app.use(jwtCheck);
+// fastify.register(jwt, { secret: 'LONG_RANDOM_VALUE' })
+// console.log(fastify.jwt.secret);
+// LONG_RANDOM_VALUE
+// var jwtCheck = jwt({
+//   secret: jwks.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: "https://dev-q-csylwc.us.auth0.com/.well-known/jwks.json",
+//   }),
+//   audience: "admin-api-platzily.com",
+//   issuer: "https://dev-q-csylwc.us.auth0.com/",
+//   algorithms: ["RS256"],
+// });
+
+// fastify.register(jwtCheck);
+// app.use(jwtCheck);
 
 // Declare a route
 fastify.get("/", async (request, reply) => {
   return { hello: "world" };
 });
+
+fastify.register(function(instance, _options, done) {
+  instance.get('/products', {
+    handler: function(request, reply) {
+      reply.send(request.user)
+    },
+    preValidation: instance.authenticate
+  })
+
+  done()
+})
 
 // Run the server!
 const start = async () => {
--- packages/web/pages/api/products.js
+++ packages/web/pages/api/products.js
@@ -1,21 +1,19 @@
 // pages/api/products.js
-import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withApiAuthRequired(async function products(req, res) {
   // If your Access Token is expired and you have a Refresh Token
   // `getAccessToken` will fetch you a new one using the `refresh_token` grant
   const { accessToken } = await getAccessToken(req, res, {
-    scopes: ["read:products"],
+    // scopes: ['read:products'],
+    permissions: ['read:products'],
   });
-
   console.log(accessToken);
-  // const response = await fetch("https://api.example.com/products", {
-  //   headers: {
-  //     Authorization: `Bearer ${accessToken}`,
-  //   },
-  // });
-  // const products = await response.json();
-  res.status(200).json({
-    message: "Hello from the server!",
+  const response = await fetch('http://localhost:3010/products', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
   });
+  const products = await response.json();
+  res.status(200).json(products);
 });
